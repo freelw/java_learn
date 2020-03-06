452c8b78dd7afad518e62bef5f8e77b8a36c28aa
liwang <liwang112358@gmail.com>
2020-03-06 14:04:09 +0800

    update

---
+++ 2/Test.java
@@ -0,0 +1,22 @@
+public class Test{
+    public static void main(String[] args) {
+        byte b = 1;
+        System.out.println(b);
+        short s = 2;
+        System.out.println(s);
+        int i = 4;
+        System.out.println(i);
+        long l = 79l;
+        System.out.println(l);
+        float f = 1.2f;
+        System.out.println(f);
+        double d = 2.2;
+        System.out.println(d);
+        char c = 'a';
+        System.out.println(c);
+        boolean bl0 = true;
+        System.out.println(bl0);
+        boolean bl1 = false;
+        System.out.println(bl1);
+    }
+}
